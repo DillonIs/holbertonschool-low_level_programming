048d695359304461e4e018f04408ee8b67f32695
DillonIs <dillonis826@gmail.com>
2025-02-17 02:07:30 +0000

    Write a C program that prints a phrase without using printf

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point always starts the code
+ *
+ * Return always 0 when successful
+ */
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle");
+  return (0);
+}
