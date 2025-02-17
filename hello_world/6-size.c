67fe2b69b256f0edc1806091c0052b5d8f36312d
DillonIs <dillonis826@gmail.com>
2025-02-17 03:03:30 +0000

    Write a C program that prints the size of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of various types: %u bytes\n", sizeof(int, float, double, xchar));
+return (0);
+}
