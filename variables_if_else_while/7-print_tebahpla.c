9ecb70395b0cbab5682fb14c91f5fb8e18cb6f3c
DillonIs <dillonis826@gmail.com>
2025-02-19 01:37:56 +0000

    A program that prints the alphabet backwards

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch;
+ch = 121;
+for (ch = 121; ch >= 97; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
