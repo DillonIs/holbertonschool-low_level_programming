10335ce0aab7539e0e443a0275deef757310dc8a
DillonIs <dillonis826@gmail.com>
2025-02-19 01:55:41 +0000

    A program that prints all the numbers in base 16 lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  int n, ch;
+  ch = 97;
+  for (n = '0'; n <= '9'; n++)
+    { putchar(n);
+    }
+  for (ch = 97; ch <= 102; ch++)
+   putchar(ch);
+ putchar('\n');
+ return (0);
+}
