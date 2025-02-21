e173ef17b97f3e6b6275a98367f02e89d8d67e7f
DillonIs <dillonis826@gmail.com>
2025-02-21 02:52:30 +0000

    Added a program that finds if c is lowercase or not

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,9 +3,10 @@
 /**
  * _islower - finds if c is lowercase
  *
+ * @c: c is the letter being tested
+ *
  * Return: 1 if c is lowercase
  *
- * Return: 0 if c isn't lowercase
  */
 
 int _islower(int c)
