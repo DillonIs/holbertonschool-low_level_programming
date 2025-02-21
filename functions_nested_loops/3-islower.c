0c4f006b3fd4d9867665f60ed2b695b8956c9565
DillonIs <dillonis826@gmail.com>
2025-02-21 02:44:00 +0000

    Added a program that finds if c is lowercase or not

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - finds if c is lowercase 
+ *
+ * Return: 1 if c is lowercase
+ *
+ * Return: 0 if c isn't lowercase
+ */
+
+int _islower(int c)
+{
+
+if (c >= 97 && c <= 122)
+  
+return (1);
+  
+else
+return (0);
+}
+
