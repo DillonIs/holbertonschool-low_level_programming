0b65f4ba679350885d9447e70864ec967e05d9fb
DillonIs <dillonis826@gmail.com>
2025-02-23 05:01:20 +0000

    Added a program that prints a times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,18 +1,40 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- *
- *
+ * times_table - multiplication table
  *
  */
 
 void times_table(void)
 {
-  int n;
-  for (n = 0; n <= 9; n++)
-    {
-      _putchar(n + 48);
-      _putchar('\n');
-    }
+int n, x;
+int y;
+for (n = 0; n <= 9; n++)
+{
+for (x = 0; x <= 9; x++)
+{
+y = n * x;
+if (x == 0)
+{
+_putchar((y % 10) + '0');
+}
+else if (y >= 10)
+{
+_putchar(' ');
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+}
+else
+{
+_putchar(' ');
+_putchar(' ');
+_putchar((y % 10) + '0');
+}
+if (x < 9)
+{
+_putchar(',');
+}
+}
+_putchar('\n');
+}
 }
