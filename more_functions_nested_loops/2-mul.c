003c27c42757ab776cdc9c182cdf351d114c1386
DillonIs <dillonis826@gmail.com>
2025-02-24 00:02:41 +0000

    Added a program that multiplies two numbers together

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers together
+ *
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: Returns the answer
+ */
+
+int mul(int a, int b)
+{
+int x;
+x = a * b;
+return (x);
+}
