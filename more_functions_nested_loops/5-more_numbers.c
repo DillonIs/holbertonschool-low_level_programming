a7d7054c269f46d986614969934b4280f6e08090
DillonIs <dillonis826@gmail.com>
2025-02-24 06:10:43 +0000

    Added a program that prints past 9 up to 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers above 9
+ *
+ * stop - ends the program
+ *
+ * n - numbers
+ *
+ * Return: No return type (void)
+ */
+
+void more_numbers(void)
+{
+int stop, n;
+stop = 10;
+n = 0;
+while (stop >= 0)
+{
+while (n <= 14)
+{
+if (n > 9)
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+n++;
+}
+n = 0;
+_putchar('\n');
+stop--;
+}
+}
