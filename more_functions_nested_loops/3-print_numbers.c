e5f3d0c42577227135dda03fe523d95ba5a93a27
DillonIs <dillonis826@gmail.com>
2025-02-24 00:22:21 +0000

    Added a program that prints 0 - 9 with only _putchar

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * n - value of numbers
+ *
+ */
+
+void print_numbers(void)
+{
+char n = '0';
+while (n <= 57)
+{
+_putchar(n);
+n++;
+}
+_putchar('\n');
+}
