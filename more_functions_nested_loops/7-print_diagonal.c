9600e75ade541f1d26e72475d5c81c9a459a671b
DillonIs <dillonis826@gmail.com>
2025-02-25 02:51:45 +0000

    Testing checker

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,20 +8,26 @@
 
 void print_diagonal(int n)
 {
-int amtrow = 0;
-int blankspace = 1;
-if (n > 0)
-while (amtrow < n)
-{
-blankspace = 0;
-while (blankspace < amtrow)
-{
-_putchar(' ');
-blankspace = blankspace + 1;
-}
-_putchar('\\');
-_putchar('\n');
-amtrow = amtrow + 1;
-}
-_putchar('\n');
+  int amtrow = 1;
+  int blankspace;
+  if (n <= 0)
+  {
+    _putchar('\n');
+  }
+  else
+    {
+      while (amtrow <= n)
+	{
+	  blankspace = 1;
+	  
+	  while (blankspace < amtrow)
+	    {
+	      _putchar(' ');
+	      blankspace++;
+	    }
+	    _putchar('\\');
+	    _putchar('\n');
+	    amtrow++;
+	}
+    }
 }
