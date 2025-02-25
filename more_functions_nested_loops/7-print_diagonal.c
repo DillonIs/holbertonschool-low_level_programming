41e59d643f53282b27ea283bc7feace5b52fc37f
DillonIs <dillonis826@gmail.com>
2025-02-25 02:22:42 +0000

    Added a program that prints / for amount of n and spaces

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - diagonal line
+ * amtrow - amount of rows
+ * blankspace - amount of spaces
+ * @n: number being tested
+ */
+
+void print_diagonal(int n)
+{
+int amtrow = 0;
+int blankspace = 0;
+if (n > 0)
+while (amtrow < n)
+{
+blankspace = 0;
+while (blankspace < amtrow)
+{
+_putchar(' ');
+blankspace = blankspace + 1;
+}
+_putchar('\\');
+_putchar('\n');
+amtrow = amtrow + 1;
+}
+_putchar('\n');
+}
