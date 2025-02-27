644b20657e0de24fff38432dc03cb651ed7cecff
DillonIs <dillonis826@gmail.com>
2025-02-27 21:33:04 +0000

    Added updates to Strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strlen: finds a strings length
- *
+ * _strlen - finds a string's length
+ * @s: string variable
  * Return: Returns stringlength
-*/
+ */
 
 int _strlen(char *s)
 {
