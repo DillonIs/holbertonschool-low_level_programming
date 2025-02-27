3ec82fffed0577fa0c6cf5b3fa6d8c8cd89f9919
DillonIs <dillonis826@gmail.com>
2025-02-27 02:24:14 +0000

    Saved

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen: finds a strings length
+ * 
+ * Return: Returns stringlength 
+ */
+
+int _strlen(char *s)
+{
+int stringlength = 0;
+
+while (*s++)
+stringlength++;
+
+return stringlength;
+}
