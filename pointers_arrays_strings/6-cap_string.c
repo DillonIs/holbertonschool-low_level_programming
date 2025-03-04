a359a8e29b4421bcbf60079bb2010a471ded8650
DillonIs <dillonis826@gmail.com>
2025-03-04 03:27:14 +0000

    Save before checked task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,26 +12,30 @@ char *cap_string(char *str)
 {
 int index = 0;
 int indexspecial = 0;
-char *separators = "\t\n,;. !?\"(){}";
-
+char *separators = " \t\n,.;!?\"(){}";
+ 
 if (index <= 0)
-{
-if (str[index] >= 'a' && str[index] <= 'z')
-{
-str[index] = str[index] - 32;
-}
-index++;
-}
-while (str[index] != '\0')
-{
-if (str[index - 1] == separators[indexspecial])
-{
-if (str[index] >= 'a' && str[index] <= 'z')
-{
-str[index] = str[index] - 32;
-}
-}
-index++;
-}
-return (str);
+  {
+    if (str[index] >= 'a' && str[index] <= 'z')
+      {
+	str[index] = str[index] - 32;
+      }
+    index++;
+  }
+ while (str[index] != '\0')
+   {
+     while (separators[indexspecial] != '\0')
+       {
+	 if (str[index - 1] == separators[indexspecial])
+	   {
+	     if (str[index] >= 'a' && str[index] <= 'z')
+	       {
+		 str[index] = str[index] - 32;
+	       }
+	   }
+	 indexspecial++;
+       }
+	 index++;
+   }
+ return (str);
 }
