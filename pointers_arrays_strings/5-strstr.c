2c89f01c8165a878698b747a8554c593d28ac750
DillonIs <dillonis826@gmail.com>
2025-03-07 23:01:56 +0000

    Added a program that finds and prints a substring in the main string

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,43 @@
 #include "main.h"
 
 /**
+ * _strstr - Function finds first occurence of substring in string
+ * @haystack: Main string set
+ * @needle: Sub-string to be found in main
  *
- *
- *
- *
+ * Return: Returns the substring if found
+ * Otherwise returns NULL
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-  
+int hayindex = 0;
+int needindex = 0;
+
+if (needle[0] == '\0')
+{
+return (haystack);
+}
+while (haystack[hayindex] != '\0')
+{
+if (haystack[hayindex] == needle[needindex])
+{
+needindex = 0;
+while (haystack[hayindex] == needle[needindex])
+{
+needindex++;
+hayindex++;
+if (needle[needindex] == '\0')
+{
+return (haystack + (hayindex - needindex));
+}
+if (haystack[hayindex] != needle[needindex])
+{
+break;
+}
+}
+}
+hayindex++;
+}
+return (0);
+}
