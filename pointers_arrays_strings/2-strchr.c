7203d2b270cad6896637b88816f9cf4ae81e3413
DillonIs <dillonis826@gmail.com>
2025-03-07 01:00:15 +0000

    Added a program that prints the string after the first occurence of a character

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - Finds and prints after first occurence of c
@@ -13,17 +12,17 @@ char *_strchr(char *s, char c)
 {
 int index = 0;
 
+if (c == '\0')
+{
+return (0);
+}
 while (s[index] != '\0')
 {
 if (s[index] == c)
 {
 return (s + index);
 }
-if (s[index] == 0)
-{
-return (NULL);
-}
 index++;
 }
-return (NULL);
+return (0);
 }
