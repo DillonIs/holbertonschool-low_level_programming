17e9c3182d4337409a854c267ba998f20782f56d
DillonIs <dillonis826@gmail.com>
2025-03-08 22:42:59 +0000

    Added a program that prints the diagonal sum of a 2D array

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,12 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *
- *
- *
+ * print_diagsums - Prints the sum of diagonal array locations
+ * @a: The array identifier
+ * @size: The size of the array
  *
  */
 
 void print_diagsums(int *a, int size)
 {
-  
+int rows = 0;
+int firstsum = 0;
+int secondsum = 0;
+
+while (rows < size)
+{
+firstsum += a[rows * size + rows];
+secondsum += a[rows * size + (size - rows - 1)];
+rows++;
+}
+printf("%d, %d\n", firstsum, secondsum);
+}
