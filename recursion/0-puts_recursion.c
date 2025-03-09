47f127ac60971cfb6c9e827a2e60496dda6db744
DillonIs <dillonis826@gmail.com>
2025-03-09 22:46:09 +0000

    Added a recursive function that prints a string

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
+ * _put_recursion - Recursive function for puts
+ * Prints a string followed by a newline
  *
- *
- *
- *
+ * @s: String identifier as pointer
  */
 
 void _puts_recursion(char *s)
 {
-  
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
