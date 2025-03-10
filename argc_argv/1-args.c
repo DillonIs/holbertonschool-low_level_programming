203d329d911fc654a3ab1f415ad85a49892dfd77
DillonIs <dillonis826@gmail.com>
2025-03-10 23:21:00 +0000

    Added a program that prints the amount of commands issued

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * @argc: Number of commands issued
+ * @argv: NULL for this program
+ *
+ * Return: Returns back to 0.
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+(void)argv;
+return (0);
+}
