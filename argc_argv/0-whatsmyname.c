35829bee5ccf580282b1f8d0a241f7bc8562ff18
DillonIs <dillonis826@gmail.com>
2025-03-10 22:56:44 +0000

    Added a program that prints the file name regardless if it is changed

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * @argc: The number of commands
+ * @argv: The array
+ *
+ * Return: Return void
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+(void)argc;
+return (0);
+}
