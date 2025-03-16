6cb18aa5ba2e9b36cfd57b8b90a8e84cd958b284
DillonIs <dillonis826@gmail.com>
2025-03-16 23:29:14 +0000

    Added a structure for dog as a header file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - Identifies which dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
+
+
+#endif
