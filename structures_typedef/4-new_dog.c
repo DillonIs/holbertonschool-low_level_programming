5c361c25722974ac8f1c49e4171622417451f1cd
DillonIs <dillonis826@gmail.com>
2025-03-17 00:43:06 +0000

    Added a function that creates a new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include "dog.h"
+/**
+ * new_dog - Creates a new dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ *
+ * Return: Returns the new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *new_dog;
+int nameindex = 0;
+int ownindex = 0;
+
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+{
+return (NULL);
+}
+new_dog->name = malloc(sizeof(name) + 1);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+new_dog->owner = malloc(sizeof(owner) + 1);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+while (nameindex != '\0')
+{
+new_dog->name[nameindex] = name[nameindex];
+nameindex++;
+}
+new_dog->name[nameindex] = '\0';
+new_dog->age = age;
+while (ownindex != '\0')
+{
+new_dog->owner[ownindex] = owner[ownindex];
+ownindex++;
+}
+new_dog->owner[ownindex] = '\0';
+return (new_dog);
+}
