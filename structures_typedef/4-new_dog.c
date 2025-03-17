d46849eb62864dce854a0bdc388101cc530a8f88
DillonIs <dillonis826@gmail.com>
2025-03-17 01:42:39 +0000

    Attempt to fix issues with task 4 creating a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,39 +10,42 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *new_dog;
-int nameindex = 0;
-int ownindex = 0;
+int nameindex;
+int ownindex;
+int i, j;
 
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 {
 return (NULL);
 }
-new_dog->name = malloc(sizeof(name) + 1);
+for (nameindex = 0; name[nameindex] != '\0'; nameindex++)
+;
+for (ownindex = 0; owner[ownindex] != '\0'; ownindex++)
+;
+new_dog->name = malloc(nameindex + 1);
 if (new_dog->name == NULL)
 {
 free(new_dog);
 return (NULL);
 }
-new_dog->owner = malloc(sizeof(owner) + 1);
+new_dog->owner = malloc(ownindex + 1);
 if (new_dog->owner == NULL)
 {
 free(new_dog->name);
 free(new_dog);
 return (NULL);
 }
-while (name[nameindex] != '\0')
+for (i = 0; i < nameindex; i++)
 {
-new_dog->name[nameindex] = name[nameindex];
-nameindex++;
+new_dog->name[i] = name[i];
 }
-new_dog->name = name;
+new_dog->name[nameindex] = '\0';
 new_dog->age = age;
-while (owner[ownindex] != '\0')
+for (j = 0; j < ownindex; j++)
 {
-new_dog->owner[ownindex] = owner[ownindex];
-ownindex++;
+new_dog->owner[j] = owner[j];
 }
-new_dog->owner = owner;
+new_dog->owner[ownindex] = '\0';
 return (new_dog);
 }
