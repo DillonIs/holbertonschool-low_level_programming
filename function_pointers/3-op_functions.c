d08ba7d46e9b9599a4f18a5410006ed61c18211d
DillonIs <dillonis826@gmail.com>
2025-03-20 03:19:36 +0000

    Save 2 after adding operation file

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,9 +1,67 @@
-#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
+ * op_add - Operation addition
+ * @a: First variable
+ * @b: Second variable
  *
- *
- *
+ * Return: Returns value of operation
  */
 
+int op_add(int a, int b)
+{
+  return (a + b);
+}
 
+/**
+ * op_sub - Operation subtraction
+ * @a: First variable
+ * @b: Second variable
+ *
+ * Return: Returns value of operation
+ */
+
+int op_sub(int a, int b)
+{
+  return (a - b);
+}
+
+/**
+ * op_mul - Operation multiplication
+ * @a: First variable
+ * @b: Second variable
+ *
+ * Return: Returns value of operation
+ */
+
+int op_mul(int a, int b)
+{
+  return (a * b);
+}
+
+/**
+ * op_div - Operation division
+ * @a: First variable
+ * @b: Second variable
+ *
+ * Return: Returns the value of operation
+ */
+
+int op_div(int a, int b)
+{
+  return (a / b);
+}
+
+/**
+ * op_mod - Operation modulo
+ * @a: First variable
+ * @b: Second variable
+ *
+ * Return: Returns value of operation 
+ */
+
+int op_mod(int a, int b)
+{
+  return (a % b);
+}
