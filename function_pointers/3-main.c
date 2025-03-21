423f67bcac378d22117c3cf6fba3191a83c0b587
DillonIs <dillonis826@gmail.com>
2025-03-21 23:42:14 +0000

    Updated 3-main so that multiple operations can't be passed

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 {
 int a, b;
 int (*operator)(int a, int b);
-  
+
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
 operator = get_op_func(argv[2]);
-if (operator == NULL)
+if (operator == NULL || !operator)
 {
 printf("Error\n");
 exit(99);
