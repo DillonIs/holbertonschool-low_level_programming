e43d00d37865fa43fcb7addb60765861c8c5a9b9
DillonIs <dillonis826@gmail.com>
2025-03-23 23:41:32 +0000

    Added a variadic function that prints strings

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,12 +3,36 @@
 #include "variadic_functions.h"
 
 /**
- *
- *
- *
+ * print_strings - Print strings in order
+ * @separator: Separates the inputs
+ * @n: The amount of arguments
  *
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-  
+va_list list;
+unsigned int index = 0;
+
+va_start(list, n);
+while (index < n)
+{
+char *string;
+string = va_arg(list, char *);
+if (string == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("%s", string);
+}
+if (separator != NULL && (index + 1) != n)
+{
+printf("%s", separator);
+}
+index++;
+}
+printf("\n");
+va_end(list);
+}
