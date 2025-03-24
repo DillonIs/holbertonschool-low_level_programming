df907b8d1aab0e3d47222da7c3b9864fac27a368
DillonIs <dillonis826@gmail.com>
2025-03-24 00:59:14 +0000

    Added a variadic function that prints anything

---
+++ variadic_functions/print_functions.h
@@ -0,0 +1,36 @@
+#ifndef PRINT_FUNCTIONS_H
+#define PRINT_FUNCTIONS_H
+
+void print_string(va_list arg)
+{
+  char *str;
+  str = va_arg(arg, char *);
+
+  printf("%s", str);
+}
+
+void print_letter(va_list arg)
+{
+  char str;
+  str = va_arg(arg, int);
+
+  printf("%c", str);
+}
+
+void print_integer(va_list arg)
+{
+  int num;
+  num = va_arg(arg, int);
+
+  printf("%d", num);
+}
+
+void print_float(va_list arg)
+{
+  float num;
+  num = va_arg(arg, double);
+
+  printf("%f", num);
+}
+
+#endif
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,21 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_string(va_list arg);
+void print_letter(va_list arg);
+void print_integer(va_list arg);
+void print_float(va_list arg);
+void print_all(const char * const format, ...);
+typedef struct print
+{
+  char *specifer;
+  void (*print_func)(va_list arg);
+} print_t;
 
 #endif
