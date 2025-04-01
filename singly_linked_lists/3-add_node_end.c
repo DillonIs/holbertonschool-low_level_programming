e0933d34b15ed6073463d9d55ec3879d2e24c299
DillonIs <dillonis826@gmail.com>
2025-04-01 00:58:20 +0000

    Save need to do task 3 and 4

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *list, *last_node;
+
+	last_node = malloc(sizeof(list_t));
+
+	
+}
+
