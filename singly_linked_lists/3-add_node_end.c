523595dda4b17f4453c7f8eb0a63b41eff407bd0
DillonIs <dillonis826@gmail.com>
2025-04-02 01:05:03 +0000

    Added a function that adds new nodes to the end of linked lists

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,17 +4,59 @@
 #include "lists.h"
 
 /**
+ * _strlen - Finds the full length
+ * @str: Pointer to struct
  *
+ * Return: Returns the length of string
+ */
+
+int _strlen(const char *str)
+{
+	unsigned int strlength = 0;
+
+	while (str[strlength] != '\0')
+	{
+		strlength++;
+	}
+	return (strlength);
+}
+
+/**
+ * add_node_end - Adds a new node to the end
+ * @head: Double pointer to each node
+ * @str: Pointer to struct
  *
- *
+ * Return: Address of new node, or NULL
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *list, *last_node;
+	list_t *list, *last;
 
-	last_node = malloc(sizeof(list_t));
+	last = malloc(sizeof(list_t));
 
-	
+	if (last == NULL)
+	{
+		return (NULL);
+	}
+	last->str = strdup(str);
+	last->len = _strlen(str);
+	last->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = last;
+	}
+	else
+	{
+		list = *head;
+
+		while (list->next)
+		{
+			list = list->next;
+		}
+		list->next = last;
+	}
+	return (last);
 }
 
