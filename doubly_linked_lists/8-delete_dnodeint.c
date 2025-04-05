9057adf33f03774eabe929f2c9b21e0eb0ac6fbc
DillonIs <dillonis826@gmail.com>
2025-04-05 00:53:47 +0000

    Added a function that deletes after a specified node in doubly linked lists

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -40,7 +40,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		return (-1);
 	}
-	if (temp->next->next == NULL || index + 1)
+	if (temp->next->next == NULL)
 	{
 		temp->next = NULL;
 	}
