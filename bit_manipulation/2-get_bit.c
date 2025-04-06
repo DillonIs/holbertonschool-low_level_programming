d8b16ef114e48a8cf083c32cc8d98e4e8d9a6c85
DillonIs <dillonis826@gmail.com>
2025-04-06 00:45:14 +0000

    Added a function that returns the value of a bit at an index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	if (index > sizeof(n) * 8)
+	{
+		return (-1);
+	}
+
+	mask <<= index;
+
+	if (mask & n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
