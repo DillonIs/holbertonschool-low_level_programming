0d993ff196ba888d5ca3539f361d0bba85429779
DillonIs <dillonis826@gmail.com>
2025-04-06 00:47:50 +0000

    Added a function that returns the value of a bit at an index

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
+ * get_bit - Finds value of a bit of an index
+ * @n: Value of long int
+ * @index: Position of bit to display
  *
- *
- *
- *
+ * Return: Returns 1 or 0 if success otherwise -1
  */
 
 int get_bit(unsigned long int n, unsigned int index)
